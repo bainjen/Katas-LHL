f225db2d7646e609a14ba9de208c1d8dc8eb75b4
Jennifer Fleming Bain <jennifer.fleming.bain@gmail.com>
2020-06-30 00:13:10 +0000

    create kata 5 and begin work

---
+++ kata5.js
@@ -0,0 +1,22 @@
+//given string of words -> turn into percent encoded string by replacing white space with %20
+
+// INSTRUCTIONS
+// Create a function named urlEncode that will receive a string of words, and return that string with all of the whitespace characters converted to %20. If there is whitespace on the outside of the string, like in our second example above, you should only replace the whitespace within the string.
+
+// Use some sort of looping. Do Not use String.prototype.replace
+
+const urlEncode = function(text) {
+  // Put your solution here
+};
+
+console.log(urlEncode("Lighthouse Labs"));
+console.log(urlEncode(" Lighthouse Labs "));
+console.log(urlEncode("blue is greener than purple for sure"));
+
+//EXPECTED OUTCOME 
+// Lighthouse%20Labs
+// Lighthouse%20Labs
+// blue%20is%20greener%20than%20purple%20for%20sure
+
+// INITIAL IDEAS:
+// could convert the string to an array and loop through to see if the index is equal to a blank space then remove that character and add in %20. Then convert the array back into a string. 
