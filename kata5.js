1d4c702eb0b98b6d5ca45e7f2f3f3ac9695af0c3
Jennifer Fleming Bain <jennifer.fleming.bain@gmail.com>
2020-07-01 00:05:59 +0000

    finalize code for kata 5

--- kata5.js
+++ kata5.js
@@ -7,26 +7,47 @@
 
 const urlEncode = function (text) {
   let replacement = "%20"
-  let newString = [];
+  let newStringArr = [];
+
   for (let i = 0; i < text.length; i++) {
     let character = text[i];
-    if (character === " ") {
-      newString.push(replacement);
+    if(character === " ") {
+      newStringArr.push(replacement);
     } else {
-      newString.push(character); 
+      newStringArr.push(character); 
     }
   }
-  console.log(newString);
+
+  if (newStringArr[0] === replacement) {
+    newStringArr.shift(); 
+  }
+  if (newStringArr[newStringArr.length - 1] === replacement) {
+    newStringArr.pop(); 
+  }
+
+  let finalString = newStringArr.join("");
+  return finalString; 
 };
 
 console.log(urlEncode("Lighthouse Labs"));
 console.log(urlEncode(" Lighthouse Labs "));
 console.log(urlEncode("blue is greener than purple for sure"));
+console.log(urlEncode("just makin' sure, ya know!? !? ! ? "));
 
 //EXPECTED OUTCOME 
 // Lighthouse%20Labs
 // Lighthouse%20Labs
 // blue%20is%20greener%20than%20purple%20for%20sure
+
+//CONSOLE LOGS TO DEBUG
+  // console.log(newString[-1]);
+  // console.log(newString.length - 1); 
+  // console.log(newString[newString.length - 1]);
+  // console.log(newStringArr);
+  // let newStr = newStringArr.toString("");
+  // console.log(newStr);
+  // console.log(newStringArr.join("")); 
+
 
 // INITIAL IDEAS:
 // could convert the string to an array and loop through to see if the index is equal to a blank space then remove that character and add in %20. Then convert the array back into a string. 
