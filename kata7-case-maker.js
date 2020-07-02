ce740a44428f30f3a1260bf89219feafc33f5e42
Jennifer Bain <jenniferbain@jennifers-air.lan>
2020-07-02 17:00:10 -0600

    setup kata 7 case maker

---
+++ kata7-case-maker.js
@@ -0,0 +1,15 @@
+// Create a function named camelCase that will convert a string to camel case, and return the result.
+
+const camelCase = function (input) {
+  // Your code here
+};
+
+console.log(camelCase("this is a string"));
+console.log(camelCase("loopy lighthouse"));
+console.log(camelCase("supercalifragalisticexpialidocious"));
+
+
+// Output
+// thisIsAString
+// loopyLighthouse
+// supercalifragalisticexpialidocious
