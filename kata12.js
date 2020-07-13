b866699c6e84c7596384ea1689f35161beb01f8b
Jennifer Fleming Bain <jennifer.fleming.bain@gmail.com>
2020-07-13 02:23:53 +0000

    update kata 12 before vacation, not complete

--- kata12.js
+++ kata12.js
@@ -11,11 +11,26 @@ const organizeInstructors = function(instructors) {
   let instructorsByCourse = {};
   let instructorsArray = []; 
 
-  for (let i = 0; i < instructors.length; i++) {
-    console.log(instructors[i]) 
-    let instructorObj = instructors[i];
 
+  for (let i = 0; i < instructors.length; i++) {
+
+    let instructorObj = instructors[i];
+    let courseName = instructorObj.course;
+    let instructorName = instructorObj.name; 
+    let newName = "";
+    // console.log(instructorObj, "hi!")
+ 
+
+    for (let y = 0; y < instructorObj.length; y++){
+      // console.log(instructorObj[y], "HELLO");
+      if (newName != instructorName) {
+        instructorsArray.push(instructorName)
+      }
+    }
+ 
   }
+  instructorsArray.push('Jen');
+  console.log(instructorsArray)
 
 };
 
@@ -45,3 +60,23 @@ console.log(organizeInstructors([
 
 // I need to put all names of instructors who teach the same course into an Array. 
 // then, I need to add those names into  an object where the key is the course name and value is/are the instructor(s)
+
+// if two instructors have the same course, we need to push their name to an array
+
+
+// Hey! I just completed this Kata and I think I can help. What I did was to write two very short for loops with a single instruction line in each. In the first loop I defined properties to the object using Object[property] format. This object properties have empty arrays as values. So they look like this: Object[property] = [] The properties were named after courses, so for the first list you get has two properties and three for the second one.
+
+// In the second for loop, I pushed instructor names in these arrays according to what they teach. So my instruction line in the loop looks like output[“course name”].push(“instructor name”). Since the instructors come already paired to the course they teach, the i on both sides of this command have the same value and the command pushes the instructors to the correct property.
+
+
+// if (instructorName.courseName)
+// instructorsArray.push(instructorName)
+// console.log(instructorsArray)
+// // for (let y = 0; y < instructors.length; y++) {
+// //   instructorsArray.push(instructorName)
+// //   console.log(instructorsArray)
+
+// // // }
+
+// courseName = instructorName.course 
+// console.log(courseName)
