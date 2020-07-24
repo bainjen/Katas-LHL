16698d8bd55b9552abe9ebb8df99a4878ae64ea4
Jennifer Fleming Bain <jennifer.fleming.bain@gmail.com>
2020-07-24 23:47:49 +0000

    add coordinate map step

--- kata16-queen-threat.js
+++ kata16-queen-threat.js
@@ -12,23 +12,17 @@ for (r = 0; r < squareSize; r++) {
 
 let whiteQueen = [0, 0];
 let blackQueen = [5, 7];
-  // let ex = [];
 const boardCoordinateMap = []; 
 
 for (y = 0; y < emptyBoard.length; y++) {
-    // coordOne = y;
-    // console.log(y);
-  // boardCoordinateMap.push([coordOne,]);
-  for (x = 0; x < emptyBoard.length; x++) {
-    // coordTwo = x;
-    // console.log(coordTwo + '-')
-    let positionValue = emptyBoard[y][x]; 
-    let posCoordinates = [x, y]; 
-    // console.log(posCoordinates);
-    boardCoordinateMap.push(posCoordinates); 
+  let rowOfCoord = [];
 
+  for (x = 0; x < emptyBoard[0].length; x++) {
+    let posCoordinates = [x, y]; 
+    rowOfCoord.push(posCoordinates);
+    // let positionValue = emptyBoard[y][x]; 
   }
-  
+  boardCoordinateMap.push(rowOfCoord);
 }
-  // console.log(boardCoordinateMap)
-  // console.log(ex)
+  // console.log(emptyBoard)
+  console.log(boardCoordinateMap)
