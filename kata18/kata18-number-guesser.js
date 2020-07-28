0868f169e5a8d4089d275b3243ba75ea07c74cb9
Jennifer Fleming Bain <jennifer.fleming.bain@gmail.com>
2020-07-28 18:05:48 +0000

    finish code to make number guessing game function properly in the console.

--- kata18/kata18-number-guesser.js
+++ kata18/kata18-number-guesser.js
@@ -14,21 +14,106 @@
 //  b. except if answer is not a number
 
 
-let random = Math.round(Math.random() * 100 + 1)
-
-// console.log(random);
-
+let random = Math.round(Math.random() * 100);
 let prompt = require("prompt-sync")();
 let answer = prompt(`Guess a number between 0 and 100 and random num is ${random}: `);
 let answers = []; 
-// console.log(typeof answer)
 
+while (answer != random) {
 
-while (parseInt(answer) !== random) {
-  answer = prompt('Guess again: ')
+  const answerAlreadyExists = answers.includes(answer);
+
+  if (isNaN(answer) || answer === '') {
+    answer = prompt("Oops, that's not a number. Guess a number: ");
+  }
+  
+  else if (answer < random) {
+    if (answerAlreadyExists) {
+      answer = prompt("You aready tired that! Guess a new number: ");
+    } else {
+      answers.push(answer);
+      answer = prompt("Too low! Guess again: ");
+    }
+  }
+  else if (answer > random) {
+    if (answerAlreadyExists) {
+      answer = prompt("You aready tired that! Guess a new number: ");
+    } else {
+      answers.push(answer);
+      answer = prompt("Too high! Guess again: ");
+    }
+  }
+  else {
+    answers.push(answer); 
+    answer = prompt("Something's not right. I had an error. Try again: ");
+  } 
 }
+
+const unique = [...new Set(answers)];
+let attempts = unique.length +1;
+
+if (attempts === 1) {
+  console.log(`Ding! Ding! Ding! WOAH! ${random} is correct. Did you cheat? You got that right on your first try--HIGH FIVE!`);
+}
+if (attempts === 2) {
+  console.log(`Ding! Ding! Ding! ${random} is correct You got it right on your second guess. eProps to you!`);
+}
+if (attempts > 2 && attempts < 15) {
+  console.log(`Ding! Ding! Ding! ${random} is correct. You took ${attempts} guesses to figure that out. Not too shabby!`);
+}
+if (attempts >= 15) {
+  console.log(`Ding! Ding! Ding! Finally! ${random} is correct. Are you tired yet!? Because it took ${attempts} guesses to figure that out. Time for a nap! zzz...`);
+}
+
+
 
 
 // code below (replace this example)
 // let answer = prompt("Guess a number: ");
 // console.log("You answered: " + answer);
+
+
+
+// vagrant [kata18]> node kata18-number-guesser.js 
+// Guess a number between 0 and 100 and random num is 69: 99
+// Too high! Guess again: 99
+// You aready tired that! Guess a new number: 99
+// Too high! Guess again: 99
+// You aready tired that! Guess a new number: 66
+// Too low! Guess again: 66
+// You aready tired that! Guess a new number: 66
+// Too low! Guess again: 76
+// Too high! Guess again: 44
+// Too low! Guess again: bh
+// Oops, that's not a number. Guess a number: j
+// Oops, that's not a number. Guess a number: u8
+// Oops, that's not a number. Guess a number: 98
+// Too high! Guess again: 69
+// [ '99', '99', '66', '66', '76', '44', 'bh', 'j', 'u8', '98' ]
+// Ding! Ding! Ding! 69 is correct. You took (numebr) guesses to figure that out. Not too shabby!
+
+
+
+// uhhhhh...
+
+// vagrant [kata18]> node kata18-number-guesser.js 
+// Guess a number between 0 and 100 and random num is 101: 12
+// Too low! Guess again: 111
+// Too high! Guess again: 101
+// [ '12', '111' ]
+// Ding! Ding! Ding! 101 is correct. You took (numebr) guesses to figure that out. Not too shabby!
+
+
+
+  // if (answers.includes(answer)) {
+  //   // answer = prompt("You aready tired that! Guess a new number: ")
+  //   console.log("you've already have done that")
+  // }
+
+
+  // for (let i = 0; i < answers.length; i++) {
+  //   if (answers[i] == answer ) {
+  //     answer = prompt("You aready tired that! Guess a new number: ")
+  //   }   
+  // }
+
